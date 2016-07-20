6f6cb420f76b0eb5f892fb6162cfc7a206ce664a
JEWEL AHMMED <jewelbdx@gmail.com>
2016-07-20 21:17:58 +0600

    Day 26 Added

---
+++ Day-26/index.php
@@ -0,0 +1,25 @@
+<html>
+    <head>
+        <title>CSS Text</title>
+        <link rel="stylesheet" href="style.css">
+        <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Pacifico'>
+    </head>
+    <body>
+
+        Hello world
+        <h1>Hello CSS</h1>
+        <h2>Hello HTML</h2>
+
+        <div class="text">
+            Lorem Ipsum is simply dummy text of the
+            printing and typesetting industry.
+            Lorem Ipsum has been the industry's
+            standard dummy
+        </div>
+
+
+        <a href="http://google.com">Google's Web</a>
+
+
+    </body>
+</html>
