d4b06db4bda4325d454d039cd6648a2d38b5e8d6
JEWEL AHMMED <jewelbdx@gmail.com>
2016-07-23 21:11:21 +0600

    Day 27 Added

---
+++ Day-27/index.php
@@ -0,0 +1,22 @@
+<html>
+    <head>
+        <title>title</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+
+        This is a <a href="#">Link</a>
+
+        <ol>
+            <li>Hello</li>
+            <li>World</li>
+        </ol>
+
+        <ul>
+            <li>Hello</li>
+            <li>World</li>
+        </ul>
+
+
+    </body>
+</html>
