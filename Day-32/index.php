d143e7bc688b57a92cff00696af7a8c49265527d
JEWEL AHMMED <jewelbdx@gmail.com>
2016-08-16 21:22:35 +0600

    Day 32 added

---
+++ Day-32/index.php
@@ -0,0 +1,58 @@
+<html>
+    <head>
+        <title>CSS</title>
+        <style>
+            .box {
+                width: 300px;
+                height: 300px;
+                margin: 10px;
+
+                float: left;
+
+            }
+            .box1 {
+                background-color: #FF603A;
+                width: 620px;
+                height: 100px;
+                float: none;
+            }
+
+            .box1 img {
+                width: 620px;
+                height: 100px;
+            }
+
+            .box4 img {
+                width: 620px;
+                height: 100px;
+            }
+
+            .box2 {
+                background-color: #ccc;
+            }
+            .box3 {
+                background-color: #ff0;
+            }
+            .box4 {
+                background-color: #000;
+                width: 620px;
+                height: 100px;
+                clear: left;
+            }
+        </style>
+    </head>
+    <body>
+
+        <div class="box box1">
+            <img src="img.jpg">
+        </div>
+        <div class="box box2"></div>
+        <div class="box box3"></div>
+        <div class="box box4">
+            <img src="footer.png">
+        </div>
+
+
+
+    </body>
+</html>
