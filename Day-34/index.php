8d58dff8772119f9ee3790128fd5b6d8cdb0fddf
JEWEL AHMMED <jewelbdx@gmail.com>
2016-08-21 17:37:56 +0600

    Day 34 added

---
+++ Day-34/index.php
@@ -0,0 +1,35 @@
+<html>
+    <head>
+        <title>CSS</title>
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+
+        <div class="container">
+            <div class="alert alert-success">
+                <strong>Success!</strong> This alert box could indicate a successful or positive action.
+            </div>
+
+            <div class="alert alert-warning">
+                <strong>Success!</strong> This alert box could indicate a successful or positive action.
+            </div>
+
+            <p>Search icon on a styled button:
+                <span class="glyphicon glyphicon-sunglasses"></span> Search
+            </p>
+
+            <button type="button" class="btn btn-default">Default</button>
+            <button type="button" class="btn btn-primary mybtn">Primary</button>
+            <button type="button" class="btn btn-success">Success</button>
+            <button type="button" class="btn btn-info">Info</button>
+            <button type="button" class="btn btn-warning">Warning</button>
+            <button type="button" class="btn btn-danger">Danger</button>
+            <button type="button" class="btn btn-link">Link</button>
+
+
+        </div>
+
+
+    </body>
+</html>
