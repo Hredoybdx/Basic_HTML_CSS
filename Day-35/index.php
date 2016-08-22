e2303735cd413729e4e5155b190b151aeffb56f4
JEWEL AHMMED <jewelbdx@gmail.com>
2016-08-23 00:37:58 +0600

    Day 35 Added

---
+++ Day-35/index.php
@@ -0,0 +1,93 @@
+<html>
+    <head>
+        <title>HTML & CSS Project</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <div class="continer">
+            <div class="header">
+                <img src="header.jpg" alt="Header Image">
+            </div>
+            <hr>
+
+            <div class="left_sidebar">
+
+                <div class="main_menu">
+                    <ul>
+                        <li>
+                            <a href="#">Home</a>
+                        </li>
+                        <li>
+                            <a href="#">Services</a>
+                        </li>
+                        <li>
+                            <a href="#">Product</a>
+                        </li>
+                        <li>
+                            <a href="#">About Us</a>
+                        </li>
+                        <li>
+                            <a href="#">Contact Us</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="left_adv">
+                    <img src="fall.jpg" alt="Mobile">
+                </div>
+
+            </div>
+
+            <div class="main_containt">
+
+
+                <h2>
+                    <u>
+                        This is text
+                    </u>
+                </h2>
+                <p>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                    when an unknown printer took a galley of type and scrambled it to make a type
+                    specimen book. It has survived not only five centuries, but also the leap into
+                    electronic typesetting, remaining essentially unchanged. It was popularised in
+                    the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+                    and more recently with desktop publishing software like Aldus PageMaker including
+                    versions of Lorem Ipsum.
+                </p>
+
+                <h2>
+                    <u>
+                        This is another text
+                    </u>
+                </h2>
+                <p>
+                    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                    when an unknown printer took a galley of type and scrambled it to make a type
+                    specimen book. It has survived not only five centuries, but also the leap into
+                    electronic typesetting, remaining essentially unchanged. It was popularised in
+                    the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+                    and more recently with desktop publishing software like Aldus PageMaker including
+                    versions of Lorem Ipsum.
+                </p>
+
+
+            </div>
+
+            <div class="footer">
+                <h2>Copyright &copy; 2016 My Company Name All Right Reserved.</h2>
+            </div>
+
+
+
+
+
+
+
+
+
+        </div>
+    </body>
+</html>
